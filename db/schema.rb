589bf4384f6adb5519e3a462cfd2d30cd28a10ce
Marvin <kokororok_15@yahoo.com>
2013-05-14 12:59:09 +0800

    restarted prject

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130514035905) do
+ActiveRecord::Schema.define(:version => 20130514045404) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
