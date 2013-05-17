bb22ee48479ed7eeee4a45296750cc2d2cf2cb6e
Kenneth <github@kennethcalamay.com>
2013-05-17 13:51:19 +0800

    Cleaned products controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ActiveRecord::Base
   attr_accessible :author, :borrowed, :description, :price, :publisher, :purchased_at, :purchased_on, :title
 
   has_many :borrow_histories
+
+  scope :borrowed, where(borrowed: true)
+  scope :unborrowed, where(borrowed: false)
 end
